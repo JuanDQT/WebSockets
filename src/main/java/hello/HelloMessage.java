7ccbe0d92839cc8af1ebd216e300996946788f46
Juan Quispe C <juandqt@gmail.com>
2015-07-02 20:08:15 +0200

    Initial commit

---
+++ src/main/java/hello/HelloMessage.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class HelloMessage {
+
+    private String name;
+    private String message;
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
